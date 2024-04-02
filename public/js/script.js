7efc924d24edaa2c028d5d20b438baf565e2351c
Laiba Noor <laibaa1202@gmail.com>
2024-04-02 22:28:36 +0530

    First Commit

---
+++ public/js/script.js
@@ -0,0 +1,80 @@
+
+const imageUpload = document.getElementById('imageUpload')
+
+const loaderDiv = document.getElementById('loader')
+
+Promise.all([
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models') //heavier more accurate version of tiny face detector
+]).then(start)
+
+function start() {
+   // showLoadingMessage('Please upload a photo')
+    document.body.append('Models Loaded')
+    recognizeFaces()
+}
+
+
+async function recognizeFaces() {
+    const container = document.createElement('div')
+    container.style.position = 'relative'
+    document.body.append(container)
+
+  
+
+    const labeledDescriptors = await loadLabeledImages()
+    loaderDiv.innerHTML = 'Faces Loaded. Add a picture now!'
+    console.log ('faces loaded', labeledDescriptors)
+    imageUpload.disabled = false
+    const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.6)
+
+    let image
+    let canvas
+
+    //detect faces in image
+    imageUpload.addEventListener('change', async () => {
+
+        if(image) image.remove()
+        if(canvas) canvas.remove()
+
+        image = await faceapi.bufferToImage(imageUpload.files[0])
+        container.append(image)
+
+        canvas = faceapi.createCanvasFromMedia(image)
+        container.append(canvas)
+
+        const displaySize = { width: image.width, height: image.height }
+        faceapi.matchDimensions(canvas, displaySize)
+
+        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        const results = resizedDetections.map((d) => {
+            return faceMatcher.findBestMatch(d.descriptor)
+        })
+        results.forEach( (result, i) => {
+            const box = resizedDetections[i].detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+            drawBox.draw(canvas)
+        })
+    })
+}
+
+//load faces
+function loadLabeledImages() {
+    const labels = ['Aman Gupta','Amit Jain','Anupam Mittal','Ashneer Grover','Ghazal Alagh',
+    'Namita Thapar','Peyush Bansal','Ritesh Agarwal','Vineeta Singh']
+    loaderDiv.innerHTML = 'Upload the picture of your favourite Shark from Shark Tank!!  Loading Faces.... '
+    return Promise.all(
+        labels.map(async (label)=>{
+            const descriptions = []
+            for(let i=1; i<=2; i++) {
+                const img = await faceapi.fetchImage(`../labeled_images/${label}/${i}.jpg`)
+                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                descriptions.push(detections.descriptor)
+            }
+            return new faceapi.LabeledFaceDescriptors(label, descriptions)
+        })
+    )
+}
---
+++ src/app.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const basePath = path.join(__dirname, '../public')
+
+app.use(express.static(basePath));
+
+// Redirect any requests to root to index
+
+
+//start express server
+app.listen(port, () => {
+    console.log('Server started on post ' + port)
+})
